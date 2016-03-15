be721efc6fc75d08e1420028da06453d686c7ea8
ngzHappy <819869472@qq.com>
2016-03-15 14:01:20 +0800

    +comment

--- QtUtility/LUAUtiltiy.hpp
+++ QtUtility/LUAUtiltiy.hpp
@@ -10,45 +10,47 @@ public:
     /*load utility module*/
     static void loadModule(lua_State * L);
     /*input a table out put a table*/
-    static int copyTable( lua_State * L );
+    static int copyTable(lua_State * L);
     /*input table name and table*/
-    static int showTable(lua_State * L );
+    static int showTable(lua_State * L);
     /*input table name and table*/
-    static int tableToString(lua_State * L );
+    static int tableToString(lua_State * L);
 protected:
     /*input table name and table*/
-    static void printTable(lua_State * L ,std::function<void(const std::string &)> print_) ;
+    static void printTable(lua_State * L,std::function<void(const std::string &)> print_);
 };
 
+/*这个类被设计用于表达 Function 和 Table*/
 class QTUTILITYSHARED_EXPORT LuaValue {
 public:
-    typedef int Type ;
-    enum : int{
+    typedef int Type;
+    enum : int {
         Type_NIL,
         Type_Function,
         Type_Table,
         Type_Count
     };
 private:
-    class TypePointer_{
+    class TypePointer_ {
     public:
-        char pointersSet[4];char tablePath[4];
-        TypePointer_(){
-            pointersSet[0]='p';pointersSet[1]='s';pointersSet[2]='e';pointersSet[3]='t';
-            tablePath[0]='t';tablePath[1]='p';tablePath[2]='t';tablePath[3]='h';
+        char pointersSet[4]; char tablePath[4];
+        TypePointer_() {
+            pointersSet[0]='p'; pointersSet[1]='s'; pointersSet[2]='e'; pointersSet[3]='t';
+            tablePath[0]='t'; tablePath[1]='p'; tablePath[2]='t'; tablePath[3]='h';
         }
     };
     static TypePointer_ type_pointer_[Type_Count];
-    class Data_{public:
-                std::weak_ptr<lua_State> luaState_;
-                        int type_ = Type_NIL;
-                                int pointer_ = 0;
-               };
+    class Data_ {
+    public:
+        std::weak_ptr<lua_State> luaState_;
+        int type_=Type_NIL;
+        int pointer_=0;
+    };
     std::shared_ptr<Data_> data_;
 public:
 
     LuaValue();
-    LuaValue(decltype(nullptr)){/**/}
+    LuaValue(decltype(nullptr)) {/**/ }
     virtual ~LuaValue();
 
     QVariant getValue() const;
@@ -58,37 +60,37 @@ public:
     void pushValue() const;
 
     std::shared_ptr<lua_State> getLuaState() const {
-        if(data_){return data_->luaState_.lock();}
+        if (data_) { return data_->luaState_.lock(); }
         return nullptr;
     }
 
-    void setLuaState(std::shared_ptr<lua_State> &&value_){
-        if(bool(data_)==false){data_=std::shared_ptr<Data_>(new Data_);}
+    void setLuaState(std::shared_ptr<lua_State> &&value_) {
+        if (bool(data_)==false) { data_=std::shared_ptr<Data_>(new Data_); }
         data_->luaState_=std::move(value_);
     }
 
-    void setLuaState(const std::shared_ptr<lua_State> &value_){
-        if(bool(data_)==false){data_=std::shared_ptr<Data_>(new Data_);}
-        data_->luaState_= value_ ;
+    void setLuaState(const std::shared_ptr<lua_State> &value_) {
+        if (bool(data_)==false) { data_=std::shared_ptr<Data_>(new Data_); }
+        data_->luaState_=value_;
     }
 
-    Type getType()const noexcept{return data_->type_;}
+    Type getType()const noexcept { return data_->type_; }
 
-    explicit operator bool()const{return bool(data_);}
+    explicit operator bool()const { return bool(data_); }
 
     void operator()(int argc=0);
 
 protected:
-    static constexpr void * getTablePathPointer(Type v) noexcept{return ( &(type_pointer_[v].tablePath) );}
-    static constexpr void * getTypeSetPointer(Type v) noexcept{return ( &(type_pointer_[v].pointersSet) );}
+    static constexpr void * getTablePathPointer(Type v) noexcept { return (&(type_pointer_[v].tablePath)); }
+    static constexpr void * getTypeSetPointer(Type v) noexcept { return (&(type_pointer_[v].pointersSet)); }
     void clear();
-    static void* getTypeSet(lua_State *, Type v);
-    
+    static void* getTypeSet(lua_State *,Type v);
+
 private:
     template< Type >void _setValue(lua_State *);
 };
 
-Q_DECLARE_METATYPE( LuaValue )
+Q_DECLARE_METATYPE(LuaValue)
 
 #endif // LUAUTILTIY_HPP
 
